092c907346157b28dd4ec750172de1779829a6d8
Max Gfeller <mg@cyon.ch>
2021-11-12 15:54:23 +0100

    Fix Queuable decorator: mutation not neccessary

--- src/decorators/Queueable.ts
+++ src/decorators/Queueable.ts
@@ -2,24 +2,26 @@ import { mutationKey } from '../Resource'
 
 export function Queueable (fn?: ({ pendingActions: Array<{}>, setDependency: (id: string) => void })) {
   return (target: any, key: string, descriptor: PropertyDescriptor) => {
-    const propertyDescriptor = Object.getOwnPropertyDescriptor(target, mutationKey)
-    if (!propertyDescriptor) throw new Error('No property descriptor found.')
+    const mutationPropertyDescriptor = Object.getOwnPropertyDescriptor(target, mutationKey)
 
-    const mutationFn = propertyDescriptor.value.fn
     const fn = descriptor.value
 
     descriptor.value = function (...args: any[]) {
       return target.queueAction.call(this, key, args)
     }
 
-    Object.defineProperty(descriptor.value, mutationKey, {
-      value: {
-        fn: mutationFn
-      },
-      writable: true,
-      enumerable: false,
-      configurable: false
-    })
+    if (mutationPropertyDescriptor) {
+      const mutationFn = mutationPropertyDescriptor.value.fn
+
+      Object.defineProperty(descriptor.value, mutationKey, {
+        value: {
+          fn: mutationFn
+        },
+        writable: true,
+        enumerable: false,
+        configurable: false
+      })
+    }
 
     Object.defineProperty(descriptor.value, 'originalFn', {
       value: fn,
