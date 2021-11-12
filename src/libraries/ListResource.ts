bb0b69bf5985dc738963b7c33e72d59728f8bab4
Max Gfeller <mg@cyon.ch>
2021-11-12 15:43:23 +0100

    ListResource implementation

--- src/libraries/ListResource.ts
+++ src/libraries/ListResource.ts
@@ -1,3 +1,4 @@
+import { Queueable } from '..'
 import { Resource } from '../Resource'
 
 export interface ListResourceState<T> {
@@ -29,7 +30,55 @@ export abstract class ListResource<T> extends Resource<ListResourceState<T>> {
     throw new Error('`fetchList` implementation is missing')
   }
 
-  protected async fetchOne (id: string, namespace: string = 'default'): Promise<T> {
+  protected async fetchOne (id: string): Promise<T> {
     throw new Error('`fetchOne` implementation is missing')
+  }
+
+  @Queueable()
+  async getItem (id: string): Promise<T> {
+    if (this.state.items[id]) {
+      return this.state.items[id]
+    }
+
+    const result = await this.fetchOne(id)
+    if (!result) {
+      throw new Error(`Item \`${id}\` not found`)
+    }
+
+    this.state.items[id] = result
+
+    return result
+  }
+
+  getItemLocal (id: string): T|null {
+    if (this.state.items[id]) {
+      return this.state.items[id]
+    }
+
+    return null
+  }
+
+  @Queueable()
+  async getList (from: number, to: number, namespace: string = 'default'): Promise<T[]> {
+    const result = await this.fetchList(from, to, namespace)
+
+    if (result.total && result.total !== this.state.namespaces[namespace].total) {
+      this.state.namespaces[namespace] = {
+        total: result.total,
+        items: new Array(result.total).fill(null)
+      }
+    }
+
+    result.items.forEach((item, index) => {
+      this.state.namespaces[namespace].items[from + index] = item
+    })
+
+    return this.getListLocal(from, to, namespace)
+  }
+
+  getListLocal (from: number, to: number, namespace: string = 'default'): T[] {
+    if (!this.state.namespaces[namespace].items) return []
+
+    return this.state.namespaces[namespace].items.slice(from, to)
   }
 }
