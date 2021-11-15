f429b7099eed706e365b7b6c59070fa6c78fa88b
Max Gfeller <mg@cyon.ch>
2021-11-15 17:33:39 +0100

    getReactiveState always returns the mutated reactive state

--- src/Resource.ts
+++ src/Resource.ts
@@ -331,9 +331,8 @@ export abstract class Resource<T extends Object> extends EventEmitter<ResourceEv
     return await this._queueAction(action, args)
   }
 
-  getReactiveState (mutated: boolean = true): T|Ref<T> {
-    if (mutated) return this.mutatedState as Ref<T>
-    return readonly(this.state) as T
+  getReactiveState (): Ref<T> {
+    return this.mutatedState as Ref<T>
   }
 
   getState (mutated: boolean = true): T {
