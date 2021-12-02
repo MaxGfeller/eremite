c49ccbb063a4851e33e5808f2dfc226c7cf2d732
Max Gfeller <mg@cyon.ch>
2021-12-02 11:37:01 +0100

    refactor: Make actionQueue in-memory first and persist changes back to storage

--- src/ActionQueue.ts
+++ src/ActionQueue.ts
@@ -1,6 +1,9 @@
 import PQueue from 'p-queue'
 import { v4 as uuid } from 'uuid'
 import EventEmitter from 'eventemitter3'
+import { debounce } from 'debounce'
+import { Ref, ref, unref } from '@vue/reactivity'
+import { watch } from '@vue/runtime-core'
 import { isTemporaryIdentifier, Mutation, MutationState } from '.'
 import { TemporaryIdentifier } from './Resource'
 
@@ -26,8 +29,6 @@ export class ActionQueue extends EventEmitter<ActionQueueEvents> {
   protected storeQueue: PQueue
   protected actionQueue: PQueue
   protected executeAction: (item: ActionQueueItem) => Promise<{ result: any, mutation: Mutation }>
-  protected getItem: (name: string) => Promise<any>
-  protected setItem: (name: string, value: any) => Promise<void>
   protected applyMutation: (actionId: string, resource: string, action: string, parameters: any[]) => void
   protected updateMutation: (actionId: string, resource: string, parameters: any[]) => void
   protected commitMutation: (actionId: string, resource: string, action: string, parameters: any[]) => void
@@ -38,10 +39,14 @@ export class ActionQueue extends EventEmitter<ActionQueueEvents> {
   protected temporaryIdentifiers: TemporaryIdentifier[] = []
   protected actionIdPromiseMapping: { [key: string]: Promise<any> } = {}
 
+  protected loadState: null | (() => Promise<ActionQueueItem[]>) = null
+  protected persistState: (state: ActionQueueItem[]) => Promise<void>
+  protected actionQueueItems: Ref<ActionQueueItem[]> = ref([])
+
   constructor (opts: {
+    loadState: () => Promise<ActionQueueItem[]>
+    persistState: (state: ActionQueueItem[]) => Promise<void>
     executeAction: (item: ActionQueueItem) => Promise<{ result: any, mutation: Mutation }>
-    getItem: (name: string) => Promise<any>
-    setItem: (name: string, value: any) => Promise<void>
     applyMutation: (actionId: string, resource: string, action: string, parameters: any[]) => void
     updateMutation: (actionId: string, resource: string, parameters: any[]) => void
     commitMutation: (actionId: string, resource: string, action: string, parameters: any[]) => void
@@ -52,9 +57,23 @@ export class ActionQueue extends EventEmitter<ActionQueueEvents> {
   }) {
     super()
 
+    this.persistState = debounce(async (state: ActionQueueItem[]) => {
+      await opts.persistState(state)
+        .catch((err) => {
+          throw new Error(`Failed to persist \`ActionQueue\` state: \`${err.message as string}\``)
+        })
+    }, 100)
+
+    this.loadState = opts.loadState
+    this.loadState()
+      .then((items) => {
+        this.actionQueueItems.value = items ?? []
+      })
+      .catch((err) => {
+        throw new Error(`Failed to load \`ActionQueue\` state: ${err.message as string}`)
+      })
+
     this.executeAction = opts.executeAction
-    this.getItem = opts.getItem
-    this.setItem = opts.setItem
     this.applyMutation = opts.applyMutation
     this.updateMutation = opts.updateMutation
     this.commitMutation = opts.commitMutation
@@ -64,6 +83,12 @@ export class ActionQueue extends EventEmitter<ActionQueueEvents> {
 
     this.storeQueue = new PQueue({ concurrency: 1, autoStart: true })
     this.actionQueue = new PQueue({ concurrency: opts.concurrency ?? 10, autoStart: false })
+
+    watch(this.actionQueueItems, () => {
+      if (this.persistState) {
+        void this.persistState(unref(this.actionQueueItems))
+      }
+    }, { deep: true })
   }
 
   public start (): void {
@@ -74,9 +99,8 @@ export class ActionQueue extends EventEmitter<ActionQueueEvents> {
     this.actionQueue.pause()
   }
 
-  public async getQueue (): Promise<ActionQueueItem[]> {
-    const queue: ActionQueueItem[] = (await this.getItem('actionQueue')) || []
-    return queue
+  public getQueue (): ActionQueueItem[] {
+    return unref(this.actionQueueItems)
   }
 
   public async queueAction (queueItem: ActionQueueItem): Promise<any> {
@@ -85,11 +109,7 @@ export class ActionQueue extends EventEmitter<ActionQueueEvents> {
     queueItem.maxTries = queueItem.maxTries ?? this.maxTries
     queueItem.timesTried = 0
 
-    await this.storeQueue.add(async () => {
-      const queue: ActionQueueItem[] = (await this.getItem('actionQueue')) || []
-      queue.push(queueItem)
-      await this.setItem('actionQueue', queue)
-    })
+    this.actionQueueItems.value.push(queueItem)
 
     this.applyMutation(queueItem.actionId, queueItem.resource, queueItem.action, queueItem.parameters)
 
@@ -101,42 +121,32 @@ export class ActionQueue extends EventEmitter<ActionQueueEvents> {
   public async updateQueuedAction (actionId: string, temporaryIdentifiers?: TemporaryIdentifier[]): Promise<void> {
     if (!temporaryIdentifiers) return
 
-    await this.storeQueue.add(async () => {
-      const queue: ActionQueueItem[] = (await this.getItem('actionQueue')) || []
-      const index = queue.findIndex(item => item.actionId === actionId)
-      if (index !== -1) {
-        queue[index].temporaryIdentifiers?.push(...temporaryIdentifiers)
-      }
-
-      await this.setItem('actionQueue', queue)
-    })
+    const index = this.actionQueueItems.value.findIndex(item => item.actionId === actionId)
+    if (index !== -1) {
+      this.actionQueueItems.value[index].temporaryIdentifiers?.push(...temporaryIdentifiers)
+    }
   }
 
   public async cancelAction (actionId: string): Promise<void> {
-    await this.storeQueue.add(async () => {
-      const queue: ActionQueueItem[] = (await this.getItem('actionQueue')) || []
-      const index = queue.findIndex(item => item.actionId === actionId)
-      if (index !== -1) {
-        const { resource } = queue[index]
-        queue.splice(index, 1)
-        this.cancelMutation(actionId, resource)
+    const index = this.actionQueueItems.value.findIndex(item => item.actionId === actionId)
+    if (index !== -1) {
+      const { resource } = this.actionQueueItems.value[index]
+      this.actionQueueItems.value.splice(index, 1)
+      this.cancelMutation(actionId, resource)
+    }
+
+    const dependingOn: string[] = [actionId]
+    while (true) {
+      const item = this.actionQueueItems.value.find(item => item.dependingOn && !dependingOn.includes(item.dependingOn))
+      if (!item) {
+        break
       }
 
-      const dependingOn: string[] = [actionId]
-      while (true) {
-        const item = queue.find(item => item.dependingOn && !dependingOn.includes(item.dependingOn))
-        if (!item) {
-          break
-        }
-
-        const { resource } = queue[index]
-        this.cancelMutation(item.actionId as string, resource)
-        dependingOn.push(item.actionId as string)
-        queue.splice(queue.findIndex(i => i.actionId === item.actionId, 1))
-      }
-
-      await this.setItem('actionQueue', queue)
-    })
+      const { resource } = this.actionQueueItems.value[index]
+      this.cancelMutation(item.actionId as string, resource)
+      dependingOn.push(item.actionId as string)
+      this.actionQueueItems.value.splice(this.actionQueueItems.value.findIndex(i => i.actionId === item.actionId, 1))
+    }
   }
 
   protected processParameters (parameters: any[]): any[] {
@@ -196,25 +206,17 @@ export class ActionQueue extends EventEmitter<ActionQueueEvents> {
           this.cancelMutation(actionQueueItem.actionId as string, actionQueueItem.resource)
           this.emit('error', err as Error, actionQueueItem)
 
-          await this.storeQueue.add(async () => {
-            const queue: ActionQueueItem[] = (await this.getItem('actionQueue')) || []
-            const index = queue.findIndex(item => item.actionId === actionQueueItem.actionId)
-            if (index !== -1) {
-              queue.splice(index, 1)
-              await this.setItem('actionQueue', queue)
-            }
-          })
+          const index = this.actionQueueItems.value.findIndex(item => item.actionId === actionQueueItem.actionId)
+          if (index !== -1) {
+            this.actionQueueItems.value.splice(index, 1)
+          }
 
           throw err
         } else {
-          await this.storeQueue.add(async () => {
-            const queue: ActionQueueItem[] = (await this.getItem('actionQueue')) || []
-            const index = queue.findIndex(item => item.actionId === actionQueueItem.actionId)
-            if (index !== -1) {
-              queue[index] = actionQueueItem
-              await this.setItem('actionQueue', queue)
-            }
-          })
+          const index = this.actionQueueItems.value.findIndex(item => item.actionId === actionQueueItem.actionId)
+          if (index !== -1) {
+            this.actionQueueItems.value[index] = actionQueueItem
+          }
 
           setTimeout(() => {
             void this.process(actionQueueItem)
@@ -224,19 +226,14 @@ export class ActionQueue extends EventEmitter<ActionQueueEvents> {
         }
       }
 
-      await this.storeQueue.add(async () => {
-        let queue: ActionQueueItem[] = (await this.getItem('actionQueue')) || []
-        const index = queue.findIndex(item => item.actionId === actionQueueItem.actionId)
-        if (index !== -1) {
-          queue.splice(index, 1)
-        }
+      const index = this.actionQueueItems.value.findIndex(item => item.actionId === actionQueueItem.actionId)
+      if (index !== -1) {
+        this.actionQueueItems.value.splice(index, 1)
+      }
 
-        queue = queue.map((actionQueueItem) => {
-          actionQueueItem.parameters = this.processParameters(actionQueueItem.parameters)
-          return actionQueueItem
-        })
-
-        await this.setItem('actionQueue', queue)
+      this.actionQueueItems.value = this.actionQueueItems.value.map((actionQueueItem) => {
+        actionQueueItem.parameters = this.processParameters(actionQueueItem.parameters)
+        return actionQueueItem
       })
 
       return actionResult.result
@@ -246,8 +243,7 @@ export class ActionQueue extends EventEmitter<ActionQueueEvents> {
   }
 
   async pickup (): Promise<void> {
-    const queue: ActionQueueItem[] = (await this.getItem('actionQueue')) || []
-    queue.forEach((queueItem) => {
+    this.actionQueueItems.value.forEach((queueItem) => {
       this.applyMutation(queueItem.actionId as string, queueItem.resource, queueItem.action, queueItem.parameters)
 
       void this.actionQueue.add(async () => {
